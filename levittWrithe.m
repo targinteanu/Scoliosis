6a146d487264f004e5ab3c64210ca11df346d841
targinteanu <targinteanu@outlook.com>
2019-01-27 20:08:33 -0500

    some modifications, before taking into account triangles

--- levittWrithe.m
+++ levittWrithe.m
@@ -11,7 +11,9 @@ end
 
 Writhe = 0;
 for i = range
-    for j = range
+%    for j = range
+    for j = range(1):(i-1)
+        %{
         r13 = cm(j-1,:) - cm(i-1,:);
         r14 = cm(j,:)   - cm(i-1,:);
         r23 = cm(j-1,:) - cm(i,:);
@@ -19,6 +21,13 @@ for i = range
         
         r34 = cm(j,:)   - cm(j-1,:);
         r12 = cm(i,:)   - cm(i-1,:);
+        %}
+        
+        p1 = cm(i-1,:); p2 = cm(i,:); 
+        p3 = cm(j-1,:); p4 = cm(j,:);
+        
+        r34 = p4-p3; r12 = p2-p1;
+        r13 = p3-p1; r14 = p4-p1; r23 = p3-p2; r24 = p4-p2;
         
         n = zeros(4, 3);
         n(1,:) = cross(r13, r14);
@@ -43,18 +52,29 @@ for i = range
         %%{
         N = n([2 3 4 1],:);
         as = diag(n*N');
+        %{
+        idx = find(imag(asin(as)));
+        if ~isempty(idx)
+            [i, j]
+            [n(idx,:); N(idx,:)]
+        end
+        %}
         Omega = sum(asin(as));
         Omega = Omega*sign( (cross(r34,r12)) *r13');
+        if abs(real(Omega)) > 1e-8
+            [i j]
+        end
         %}
         
         Writhe = Writhe + Omega;
     end
 end
-Writhe = Writhe/(4*pi);
-if abs(imag(Writhe)) > 4e-9
+%Writhe = Writhe/(4*pi);
+Writhe = Writhe/(2*pi);
+if abs(imag(Writhe)) > 9e-9
     disp('Error: non-real Writhe')
-%else
-%    Writhe = real(Writhe);
+else
+    Writhe = real(Writhe);
 end
 
 end
