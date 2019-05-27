4d7fc512c268f6ba6504bc9f9c88f5b9f6b04c6f
targinteanu <targinteanu@outlook.com>
2019-05-27 15:09:11 -0400

    commit before changing from table to XL

--- spineMetricViewer.m
+++ spineMetricViewer.m
@@ -9,21 +9,42 @@ load('spines_XYZ.mat');
     cm = [x;y;z]';
 
 %
+q = 4;
 tau = zeros(1, length(x)-2*q); wr = tau;
+d1 = tau; d2 = d1; d3 = d1;
 for t = 1:length(tau)
-    tau(t) = lewinerTorsion(cm, t+q, q);
+    [tau(t),d,dd,ddd] = lewinerTorsion(cm, t+q, q);
+    d1(t) = norm(d); d2(t) = norm(dd); d3(t) = norm(ddd);
     wr(t) = levittWrithe(cm, (0:(2*q)) + t);
 end
 
+vertnames = [arrayfun(@(i) ['T' num2str(i)], 1:12, 'UniformOutput', 0), ...
+    arrayfun(@(i) ['L' num2str(i)], 1:5, 'UniformOutput', 0)];
+vertnames_part = vertnames((q+1):(end-q));
+
 figure('Position', [50 50 1400 700]); 
-subplot(1,4,1); plot3(x, y, z, '-o'); grid on; view([0 0]);
+subplot(1,4,1); plot3(x, y, z, '-o'); grid on; view([0 0]); 
+title('sagittal'); xlabel('x'); ylabel('y'); zlabel('z'); 
+text(x,y,z,vertnames);
 subplot(1,4,2); plot3(x, y, z, '-o'); grid on; view([90 0]);
-subplot(1,4,3); plot(tau, z((q+1):(end-q)), '-o'); grid on;
-subplot(1,4,4); plot(-wr, z((q+1):(end-q)), '-o'); grid on;
+title('coronal'); xlabel('x'); ylabel('y'); zlabel('z'); 
+text(x,y,z,vertnames);
+%subplot(1,4,3); plot([d1;d2;d3], z((q+1):(end-q)), '-o'); grid on;
+%title('Derivatives'); ylabel('z'); xlabel('derivatives'); ylim([0 1000]);
+%legend('1st', '2nd', '3rd');
+subplot(1,4,3); plot(d1, z((q+1):(end-q)), '-o'); grid on;
+title('1st Derivative'); ylabel('z'); xlabel('1st derivative'); ylim([0 1000]);
+text(d1, z((q+1):(end-q)), vertnames_part);
+subplot(1,4,4); plot(tau, z((q+1):(end-q)), '-o'); grid on;
+title('Torsion'); ylabel('z'); xlabel('Torsion'); ylim([0 1000]);
+%subplot(1,4,4); plot(-wr, z((q+1):(end-q)), '-o'); grid on;
+%title('Writhe'); ylabel('z'); xlabel('Writhe'); ylim([0 1000]);
+text(tau, z((q+1):(end-q)), vertnames_part);
 
 %}
 
 %%
+%{
 load('spines_XYZ.mat');
 %Idx = [12, 4, 8, 17, 11, 18, 9];
 Idx = [26, 20, 16, 9, 19, 7, 5];
@@ -71,3 +92,4 @@ subplot(1,3,2); grid on; view([90 0]); legend(lbl, 'Location', 'southoutside');
 title('Coronal'); xlabel('x'); ylabel('y'); zlabel('z'); 
 subplot(1,3,3); grid on; view([0 90]); legend(lbl, 'Location', 'southoutside'); 
 title('Axial'); xlabel('x'); ylabel('y'); zlabel('z'); 
+%}
