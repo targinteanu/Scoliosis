5ef7e25edf80ece4a9b64b5cf2ab55b23160cbda
targinteanu <targinteanu@outlook.com>
2019-07-06 18:02:41 -0400

    worked on segmenting ESO scans

--- esotest.m
+++ esotest.m
@@ -9,10 +9,27 @@ figure; imshow(cortrim); figure; imshow(sagtrim);
 
 %%
 sag2 = double(sag)/255; cor2 = double(cor)/255;
+%{
 im = showreconstruct(sag2); sagbone = sag2-im; 
 im = showreconstruct(cor2); corbone = cor2-im; 
+%}
+sagim = imreconstruct(double(~sagroi), sag2); sagbone = sag2-sagim; 
+corim = imreconstruct(double(~corroi), cor2); corbone = cor2-corim; 
+sagbone = adapthisteq(sagbone); corbone = adapthisteq(corbone); 
 
-%%
+%sagspine = sagbone; corspine = corbone;
 sagspine = sagbone.*sagroi; corspine = corbone.*corroi; 
 spine = [sagspine corspine]; 
-figure; imshow(spine > .05*max(spine(:)));
+spinethresh = [spine > .03*max(spine(:)), spine > .05*max(spine(:)), spine > .07*max(spine(:))];
+%figure; imshow(spinethresh);
+figure; imshow([sag2 sagspine cor2 corspine])
+%}
+
+%%
+seg = corspine;
+CC = bwconncomp(seg > .04);
+minsz = 5; % pixels 
+sz = arrayfun(@(i) length(CC.PixelIdxList{i}), 1:length(CC.PixelIdxList));
+CC.PixelIdxList = CC.PixelIdxList(sz > minsz); 
+CC.NumObjects = length(CC.PixelIdxList);
+L = labelmatrix(CC); figure; imshow(label2rgb(L));
