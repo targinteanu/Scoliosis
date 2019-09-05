d641305704dde85ca6923386c00b8511fc109475
targinteanu <targinteanu@outlook.com>
2019-09-05 00:14:31 -0400

    added interpolation

--- spineMetricViewer.m
+++ spineMetricViewer.m
@@ -9,16 +9,39 @@ load('spines_XYZ.mat');
     cm = [x;y;z]';
 %}
 
-%[num, txt] = xlsread('Writhe-pre-post.xlsx');
-%XYZ = num(:, 6:end); 
+%{
+[num, txt] = xlsread('Writhe-pre-post.xlsx');
+XYZ = num(:, 6:end); patien
 shapecluster = num(:,3); 
 writhe_preop = num(:,4); 
 writhe_postop = num(:,5);
 nonsurg = isnan(writhe_postop);
+%}
+
+[num, txt] = xlsread('Writhe-pre-post_new-metrics.csv');
+N = 32;
+num = num(1:N, :);
+XYZ = num(:, 13:end); 
+
+shapecluster = num(:,1);
+writhe = num(:,6); abswrithe = num(:,7); 
+tor1 = num(:,8); tor2 = num(:,9); torglob = num(:,10); 
+twist = num(:,11); writhetwist = num(:,12);
+
     % get center points 
+    %{
     x = mean(XYZ(cluster_shape==2, 1:3:51)); 
     y = mean(XYZ(cluster_shape==2, 2:3:51)); 
     z = mean(XYZ(cluster_shape==2, 3:3:51)); 
+    %}
+    x = (XYZ(patient_number, 1:3:51)); 
+    y = (XYZ(patient_number, 2:3:51)); 
+    z = (XYZ(patient_number, 3:3:51)); 
+    
+    x0 = x; y0 = y; z0 = z;
+    intp = 4;
+    x = interp(x, intp); y = interp(y, intp); z = interp(z, intp);
+    
     cm = [x;y;z]';
 
 %
@@ -37,21 +60,23 @@ vertnames = [arrayfun(@(i) ['T' num2str(i)], 1:12, 'UniformOutput', 0), ...
 vertnames_part = vertnames((q+1):(end-q));
 
 figure('Position', [50 50 1400 700]); 
-subplot(1,5,1); plot3(x, y, z, '-o'); grid on; view([0 0]); 
+subplot(1,5,1); plot3(x, y, z, '.'); grid on; hold on; plot3(x0, y0, z0, 'o');
+view([0 0]); zlim([0 1000]);
 title('sagittal'); xlabel('x'); ylabel('y'); zlabel('z'); 
-text(x,y,z,vertnames);
-subplot(1,5,2); plot3(x, y, z, '-o'); grid on; view([90 0]);
+text(x0,y0,z0,vertnames);
+subplot(1,5,2); plot3(x, y, z, '.'); grid on; hold on; plot3(x0, y0, z0, 'o');
+view([90 0]); zlim([0 1000]);
 title('coronal'); xlabel('x'); ylabel('y'); zlabel('z'); 
-text(x,y,z,vertnames);
+text(x0,y0,z0,vertnames);
 %subplot(1,4,3); plot([d1;d2;d3], z((q+1):(end-q)), '-o'); grid on;
 %title('Derivatives'); ylabel('z'); xlabel('derivatives'); ylim([0 1000]);
 %legend('1st', '2nd', '3rd');
 subplot(1,5,3); plot(d1, z((q+1):(end-q)), '-o'); grid on;
-title('1st Derivative'); ylabel('z'); xlabel('2nd derivative'); ylim([0 1000]);
-text(d1, z((q+1):(end-q)), vertnames_part);
+title('1st Derivative'); ylabel('z'); xlabel('1st derivative'); ylim([0 1000]);
+%text(d1, z((q+1):(end-q)), vertnames_part);
 subplot(1,5,4); plot(d2, z((q+1):(end-q)), '-o'); grid on;
 title('2nd Derivative'); ylabel('z'); xlabel('2nd derivative'); ylim([0 1000]);
-text(d2, z((q+1):(end-q)), vertnames_part);
+%text(d2, z((q+1):(end-q)), vertnames_part);
 subplot(1,5,5); plot(tau, z((q+1):(end-q)), '-o'); grid on;
 title('torsion'); ylabel('z'); xlabel('\tau'); ylim([0 1000]);
 %subplot(1,4,4); plot(tau, z((q+1):(end-q)), '-o'); grid on;
