0085f8e9d04d48b266209e6abd0d34449b27063b
targinteanu <targinteanu@outlook.com>
2019-10-05 16:33:53 -0400

    added 3D derivative magnitudes, before lewiner analysis of spline interpolation

--- splinetest.m
+++ splinetest.m
@@ -19,7 +19,7 @@ L = sqrt(diag(pdisp*pdisp'));
 L = [0; cumsum(L)]; 
 
 nplots = 4;
-figure; subplot(1,nplots,1); plot(var, L, 'ok'); grid on; hold on; ylim([L(1), L(end)]);
+%figure; subplot(1,nplots,1); plot(var, L, 'ok'); grid on; hold on; ylim([L(1), L(end)]);
 
 %pp = [spline(L, x), spline(L, y), spline(L, z)];
 pp = spline(L, var);
@@ -48,16 +48,19 @@ end
     q = 4; % 2 vertebrae above, 2 vertebrae below, current vertebra -> 5 points to fit each cubic
     vertebrae = (1+q):(size(p,1)-q); 
     taulew = zeros(size(vertebrae)); % local torsion at each point on the spine 
-    d2x = zeros(size(vertebrae)); d1x = d2x; d3x = d2x;
+    %d2x = zeros(size(vertebrae)); d1x = d2x; d3x = d2x;
     %d2z = zeros(size(vertebrae)); d1z = d2z; d3z = d2z;
+    d2 = zeros(size(vertebrae)); d1 = d2; d3 = d2;
     for vertebra = vertebrae
         [taulew(vertebra-q), d, dd, ddd] = lewinerTorsion(p, vertebra, q);
-        d2x(vertebra-q) = dd(nvar); d1x(vertebra-q) = d(nvar); d3x(vertebra-q) = ddd(nvar);
+        %d2x(vertebra-q) = dd(nvar); d1x(vertebra-q) = d(nvar); d3x(vertebra-q) = ddd(nvar);
         %d2z(vertebra-q) = dd(3); d1z(vertebra-q) = d(3); d3z(vertebra-1) = ddd(3);
+        d1(vertebra-q) = norm(d); d2(vertebra-q) = norm(dd); d3(vertebra-q) = norm(ddd);
     end
     %d2 = d2x./d2z; d1 = d1x./d1z; d3 = d3x./d3z;
-    d2 = d2x; d1 = d1x; d3 = d3x;
-    
+    %d2 = d2x; d1 = d1x; d3 = d3x;
+
+    %{
 for i = 1:length(taulew)
     vertebra = vertebrae(i);
     t = linspace(L(vertebra-1), L(vertebra+1)); t2 = t-L(vertebra);
@@ -65,34 +68,10 @@ for i = 1:length(taulew)
     %t = linspace(L(vertebra-q), L(vertebra+q));
     plot(cub, t, 'r');
 end
+    %}
     
 df1 = deriv_front(cf, 1); df2 = deriv_front(cf, 2); df3 = deriv_front(cf, 3);
 %db1 = deriv_back(cf, 1, b); db2 = deriv_back(cf, 1, b); db3 = deriv_back(cf, 3, b);
-    
-subplot(1, nplots, 2); plot(diff(var)'./diff(L), (L(1:(end-1))+L(2:end))/2, 'ok'); 
-grid on; hold on;
-plot(dxbrute, Linterp(2:end), '--k', 'LineWidth', 1);
-ylim([L(1), L(end)]);
-plot(d1, L(vertebrae), '^r'); 
-plot(df1, L(1:(end-1)), '+b');
-%plot(db1, L(2:end), 'xb');
-
-subplot(1, nplots, 3); plot(diff(diff(var)'./diff(L))./diff(L(2:end)), L(2:(end-1)), 'ok');
-grid on; hold on;
-plot(ddxbrute, Linterp(2:(end-1)), '--k', 'LineWidth', 1);
-ylim([L(1), L(end)]);
-plot(d2, L(vertebrae), '^r'); 
-plot(df2, L(1:(end-1)), '+b');
-%plot(db2, L(2:end), 'xb');
-
-subplot(1, nplots, 4); plot( diff(diff(diff(var)'./diff(L))./diff(L(2:end)))./diff(L(2:(end-1))), ...
-    L(3:(end-1)), 'ok');
-grid on; hold on;
-plot(dddxbrute, Linterp(3:(end-1)), '--k', 'LineWidth', 1);
-ylim([L(1), L(end)]);
-plot(d3, L(vertebrae), '^r'); 
-plot(df3, L(1:(end-1)), '+b');
-%plot(db3, L(2:end), 'xb');
 
 %{
 tauspline = arrayfun(@(i) -(cross(df1(i), df2(i)) * df3(i)')/(norm(cross(df1(i), df2(i)))^2), ...
@@ -127,8 +106,52 @@ for i = 1:length(tauspline)
     tauspline(i) = -(cx * dddr(i,:)')/(norm(cx)^2);
 end
 
-figure; plot(tauspline, 1:length(tauspline), '-ob'); hold on; grid on; 
-plot(taulew, vertebrae, '-^r');
+figure; subplot(1, nplots, 1);
+plot(tauspline, L(1:(end-1)), '-ob'); hold on; grid on; 
+plot(taulew, L(vertebrae), '-^r');
+ylim([L(1), L(end)]);
+title('torsion');
+
+df1 = arrayfun(@(i) norm(dr(i,:)), 1:size(dr,1)); 
+df2 = arrayfun(@(i) norm(ddr(i,:)), 1:size(ddr,1)); 
+df3 = arrayfun(@(i) norm(dddr(i,:)), 1:size(dddr,1)); 
+
+%%
+subplot(1, nplots, 2); %plot(diff(var)'./diff(L), (L(1:(end-1))+L(2:end))/2, 'ok'); 
+grid on; hold on;
+%plot(dxbrute, Linterp(2:end), '--k', 'LineWidth', 1);
+ylim([L(1), L(end)]);
+plot(d1, L(vertebrae), '-^r'); 
+plot(df1, L(1:(end-1)), '-+b');
+%plot(db1, L(2:end), 'xb');
+title('d/ds'); 
+
+subplot(1, nplots, 3); %plot(diff(diff(var)'./diff(L))./diff(L(2:end)), L(2:(end-1)), 'ok');
+grid on; hold on;
+%plot(ddxbrute, Linterp(2:(end-1)), '--k', 'LineWidth', 1);
+ylim([L(1), L(end)]);
+plot(d2, L(vertebrae), '-^r'); 
+plot(df2, L(1:(end-1)), '-+b');
+%plot(db2, L(2:end), 'xb');
+title('d^2/ds^2');
+
+%{
+subplot(1, nplots, 4); 
+%plot( diff(diff(diff(var)'./diff(L))./diff(L(2:end)))./diff(L(2:(end-1))), L(3:(end-1)), 'ok');
+grid on; hold on;
+%plot(dddxbrute, Linterp(3:(end-1)), '--k', 'LineWidth', 1);
+ylim([L(1), L(end)]);
+plot(d3, L(vertebrae), '-^r'); 
+plot(df3, L(1:(end-1)), '-+b');
+%plot(db3, L(2:end), 'xb');
+title('d^3/ds^3');
+%}
+
+subplot(1, nplots, 4); 
+grid on; hold on;
+plot(sqrt(x.^2 + y.^2), L, '-ok');
+ylim([L(1), L(end)]);
+title('(x^2 + y^2)^{1/2}');
 
 %%    
 
