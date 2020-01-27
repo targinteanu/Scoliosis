df7a95ee86abef62d94e27fb80ed47a37d3e0da8
targinteanu <targinteanu@outlook.com>
2020-01-26 22:46:33 -0500

    fixed bug (?)

--- examplecurve_old.m
+++ examplecurve_old.m
@@ -1,20 +1,20 @@
 %% input variables: ---------------------------------------------------
-r = @(s) [sin(s/sqrt(2)), cos(s/sqrt(2)), s/sqrt(2)];
-dr = @(s) (1/sqrt(2))*[cos(s/sqrt(2)), -sin(s/sqrt(2)), 1];
+r = @(s) [0*s, 0*s, s];
+dr = @(s) [0, 0, 1];
 v = @(s) [cos(pi/2 + s*pi/(2*pi*sqrt(2))), sin(pi/2 + s*pi/(2*pi*sqrt(2))), 0];
-npts = 20;
-Smin = 0; Smax = .5*4*pi*sqrt(2);
+npts = 50;
+Smin = 0; Smax = 2*4*pi*sqrt(2);
 % --------------------------------------------------------------------
 
 %% operations
 
 S = linspace(Smin, Smax, npts)';
 vr = @(s) v(s) - proj(v(s), r(s)); ur = @(s) vr(s)/norm(vr(s));
-dr_norm = @(s) dr(s)/norm(dr(s)); 
+T = @(s) dr(s)/norm(dr(s)); % (unit) tangent vector 
 
 R = r(S); VR = cell2mat( arrayfun(vr, S, 'UniformOutput', false) );
 
-Twist_integral = integral( @(t) dTw(t, dr_norm, ur), S(1), S(end)) / (2*pi)
+Twist_integral = integral( @(t) dTw(t, T, ur, dr), S(1), S(end)) / (2*pi)
 Twist_estimate = deturckTwist2(R, VR)
 Writhe_integral = integral2( @(t1,t2) ddWr(t1,t2, r, r, dr, dr), ...
     S(1), S(end), S(1), S(end), 'Method', 'iterated') / (4*pi)
@@ -36,23 +36,34 @@ function proj_ = proj(u, x)
     end
 end
 
-    function dTw_ = dTw(t, dr, u)
+function ds_ = ds(t, dr)
+% arc length from ti to tf
+t = t';
+ds_ = zeros(size(t)); imax = length(t);
+    for i = 1:imax
+        tt = t(i);
+        ds_(i) = norm(dr(tt));
+    end
+    ds_ = ds_';
+end
+
+    function dTw_ = dTw(t, T, u, DR)
     % u, du must be entered s.t. u perp dr
+    % dr must be a unit vector 
         t = t';
-        dTw_ = zeros(size(t)); imax = length(t);
+        dTw_ = zeros(size(t)); AL = zeros(size(t));
+        U = zeros(length(t), length(u(0)));
+        imax = length(t);
         for i = 1:imax
             tt = t(i);
-            if (i == 1) 
-                if (i+1 <= imax)
-                    dtt = t(i+1)-t(i);
-                else
-                    dtt = .0001;
-                end
-            else
-                dtt = t(i)-t(i-1);
-            end
-            du = (u(tt + dtt) - u(tt))/dtt;
-            dTw_(i) = cross(dr(tt), u(tt)) * du';
+            U(i,:) = u(tt);
+            AL(i) = integral( @(ttt) ds(ttt, DR), 0, tt );
+        end
+        du = diff(U)./diff(AL);
+        du = [zeros(size(u(0))); du];
+        for i = 1:imax
+            tt = t(i);
+            dTw_(i) = cross(T(tt), u(tt)) * du(i,:)';
         end
         dTw_ = dTw_';
     end
