d526ca86fa5a3c0ea4b9e68e6e3de39406e23521
targinteanu <targinteanu@outlook.com>
2020-07-12 16:05:21 -0400

    testing kmeans with EOS

--- EOSfinder.m
+++ EOSfinder.m
@@ -3,10 +3,11 @@
 %ifo = dicominfo([fp '\' fn])
 %figure; imshow(img, [])
 fp = uigetdir; 
-fs = 1:4;
-for f = fs
+f = 1;
+while exist([fp '\(' num2str(f) ')'], 'file')
    img = dicomread([fp '\(' num2str(f) ')']); 
    sz = size(img);
        figure; imshow(img, []);
        title([num2str(f) ' ' num2str(sz(1)) ' ' num2str(sz(2))]);
+   f = f+1;
 end
