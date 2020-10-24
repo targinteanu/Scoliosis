6af26cc7c5dd49ee6dc2fd1fc311c68cb5fe6fd6
targinteanu <targinteanu@outlook.com>
2020-10-24 17:38:53 -0400

    added brightening

--- showSegmentation3D.m
+++ showSegmentation3D.m
@@ -69,10 +69,39 @@ subplot(1,2,2); imshow(label2rgb(seg));
 
 verts = unique(seg(:))
 
-%%
+%% enlarge 
 scl = 2;
 slc_large = imresize(slc, scl, 'bilinear');
 seg_large = imresize(seg,scl, 'nearest');
+
+%% brighten
+mapbrightness = @(I) 1-exp(-3*I);
+
+figure; 
+subplot(2,2,1); histogram(slc_large(:)); 
+slc_bright = mapbrightness(slc_large); 
+subplot(2,2,2); histogram(slc_bright(:));
+subplot(2,2,3); imshow(slc_large, []);
+subplot(2,2,4); imshow(slc_bright, []); 
+
+%% sharpening matrix
+figure; 
+amt = 15:5:35;
+rad = .5:.25:1.5;
+for i = 1:length(amt)
+    for j = 1:length(rad)
+        spidx = (j-1)*length(rad) + i;
+        subplot(length(rad), length(amt), spidx);
+        
+        slc_sharp = imsharpen(slc_bright, 'Amount', amt(i), 'Radius', rad(j));
+        slc_sharp(seg_large==0) = slc_bright(seg_large==0);
+        slc_sharp = slc_sharp.*(slc_sharp > 0);
+        
+        imshow(slc_sharp, []);
+    end
+end
+
+%% sharpen 
 
 %{
 k = cell(1,6);
@@ -94,30 +123,13 @@ slc_sharp = imsharpen(slc_large, 'Amount', 30, 'Radius', 1.25, 'Threshold', 0);
 slc_sharp(seg_large==0) = slc_large(seg_large==0);
 slc_sharp = slc_sharp.*(slc_sharp > 0);
 
-figure; subplot(1,2,1); imshow(slc_large, []); %subplot(2,2,3); histogram(slc(:));
+figure; subplot(1,2,1); imshow(slc_bright, []); %subplot(2,2,3); histogram(slc(:));
 subplot(1,2,2); imshow(slc_sharp, []); %subplot(2,2,4); histogram(slc_sharp(:));
 
-%%
-figure; 
-amt = 15:5:35;
-rad = .5:.25:1.5;
-for i = 1:length(amt)
-    for j = 1:length(rad)
-        spidx = (j-1)*length(amt) + i;
-        subplot(length(rad), length(amt), spidx);
-        
-        slc_sharp = imsharpen(slc_large, 'Amount', amt(i), 'Radius', rad(j));
-        slc_sharp(seg_large==0) = slc_large(seg_large==0);
-        slc_sharp = slc_sharp.*(slc_sharp > 0);
-        
-        imshow(slc_sharp, []);
-    end
-end
-
-%%
+%% show seg
 colr = {'r', 'b', 'g', 'c', 'm', 'y'};
 lsty = {'-', '--', ':', '-.'};
-figure; imshow(slc_sharp, []); hold on;
+figure; imshow(slc_bright, []); hold on;
 se = strel('sphere', seR);
 c = 1;
 for n = 1:length(vertebrae)
