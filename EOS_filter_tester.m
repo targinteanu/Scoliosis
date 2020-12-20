10d878612f3188440192177a9bacefda95d4d8e9
targinteanu <targinteanu@outlook.com>
2020-12-20 01:20:51 -0500

    calculate writhe and save file

--- EOS_filter_tester.m
+++ EOS_filter_tester.m
@@ -19,11 +19,11 @@ end
 % Sag = x; Cor = y
 
 filtfilt3d = @(filtobj, sig3d) [filtfilt(filtobj, sig3d(:,1)), filtfilt(filtobj, sig3d(:,2)), sig3d(:,3)];
-plot3d = @(sig3d) plot3(sig3d(:,1), sig3d(:,2), sig3d(:,3));
+plot3d = @(sig3d) plot3(sig3d(:,1), sig3d(:,2), sig3d(:,3), 'LineWidth', 1);
 
 figure('Position', [50, 100, 1400, 800]);
 ntot = length(patients_avail);
-ncol = 3; nrow = ceil(ntot/ncol);
+ncol = 5; nrow = ceil(ntot/ncol);
 for i = 1:ntot
     subplot(nrow, ncol, i)
     
@@ -31,10 +31,10 @@ for i = 1:ntot
     fnp = [base_fp, num2str(p), img_fp, 'patient',num2str(p),' EOSoutline data.mat'];
     load(fnp);
     
-    plot3(splSclSmp(:,1), splSclSmp(:,2), splSclSmp(:,3));
+    plot3(splSclSmp(:,1), splSclSmp(:,2), splSclSmp(:,3), ':');
     grid on; hold on;
+    plot3(splSclSmp([1,end],1), splSclSmp([1,end],2), splSclSmp([1,end],3), 'ob', 'LineWidth', 2);
     %wr = getWrithe(splSclSmp); title(num2str(wr));
-    title(num2str(patients_avail(i)));
     
     fcutoff = 1/100; %1/mm
     
@@ -54,8 +54,17 @@ for i = 1:ntot
     
     xlim([min(splSclSmp(:,1)), max(splSclSmp(:,1))]);
     ylim([min(splSclSmp(:,2)), max(splSclSmp(:,2))]);
+    view(2);
     
-    legend('orig','FIR','butter','cheby');
+    splfilt = splCHB;
+    %Wr = 0;
+    Wr = getWrithe(splfilt); %wr2 = levittWrithe(splf);
+    title([num2str(patients_avail(i)), '|', num2str(Wr)]);
+    
+    fnp2 = [base_fp, num2str(p), img_fp, 'patient',num2str(p),' filtered data.mat'];
+    save(fnp2, 'splfilt', 'Wr');
+    
+    %legend('orig','FIR','butter','cheby');
     
     %{
     [~,corFilt,~,corFS,corPoly,corZ] = ...
@@ -67,4 +76,6 @@ for i = 1:ntot
     plot3(sagFilt, corFilt, sagZ);
     plot3(sagFS, corFS, sagZ);
     %}
+    
+    i/ntot
 end
