905db460555f437ed5487648d96cfdc8f4374c71
targinteanu <targinteanu@outlook.com>
2020-12-20 15:11:04 -0500

    weighted is working

--- WLStest.m
+++ WLStest.m
@@ -1,11 +1,16 @@
-X = (0:5:50)'; Y = (X - (30*rand + 10)).*(X - (30*rand + 10)).*(X - (30*rand + 10)) + 3000*(rand(size(X))-.5);
-x = 0:50;
+X = ([0:5:50,0:5:50,0:5:50])'; 
+Y = (X - (30*rand + 10)).*(X - (30*rand + 10)).*(X - (30*rand + 10)) + 10000*(rand(size(X))-.5);
+wts = 100*rand(size(X)) + .001;
+x = 0:.1:50;
+a = (wts(1:11).*Y(1:11) + wts(12:22).*Y(12:22) + wts(23:33).*Y(23:33))./(wts(1:11)+wts(12:22)+wts(23:33));
 
 coeff2fun = @(coeff, x) arrayfun(@(i) sum(coeff.*(x(i).^(0:(length(coeff)-1)))), 1:length(x));
-b = WLS(X,Y,ones(size(X)),3)
+b = WLS(X,Y,wts,10);
 y = coeff2fun(b', x); 
 
-figure; plot(X,Y,'o'); hold on; plot(x, y);
+figure; errorbar(X,Y, 10000./wts, 'o');
+hold on; plot(x, y);
+hold on; plot(X(1:11), a, '*')
 
 function beta = WLS(x,y,w,N)
     X = cell2mat(arrayfun(@(xi) xi.^(0:N), x, 'UniformOutput', false));
